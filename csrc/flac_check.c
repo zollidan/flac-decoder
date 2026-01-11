c556c1cc9c666bdb307d3f7cd5023250f92af869
Stepan Fedosov <zollidan@yandex.ru>
2026-01-12 02:30:38 +0300

    add: flex code on C just for practice

---
+++ csrc/flac_check.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void welcome_code() {
+    printf("Hello from C code!");
+}
+
+
+int check_flac_header(int file) {
+    return 0;
+}
---
+++ csrc/flac_check.h
@@ -0,0 +1,8 @@
+#ifndef FLAC_CHECK_C
+#define FLAC_CHECK_C
+
+#include <stdio.h>
+
+int check_flac_header()
+
+#endif // FLAC_CHECK_C
